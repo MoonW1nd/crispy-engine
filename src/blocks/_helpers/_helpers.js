a1bb277b626bcef0d0d171bd39bc084c91f0b1d7
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-10-06 16:57:21 +0300

    Adaptive image create

--- src/blocks/_helpers/_helpers.js
+++ src/blocks/_helpers/_helpers.js
@@ -7,5 +7,14 @@ export function getTemplateContent(templateClassName) {
 
 
 export function getImageHtml(nameImage, extension, className, alt = null) {
+  if (extension === 'jpg' || extension === 'png') {
+    return `<img src="./assets/${nameImage}.${extension}" class="${className}"
+      srcset="
+      ./assets/${nameImage}.${extension} 1x,
+      ./assets/${nameImage}-x2.${extension} 2x,
+      ./assets/${nameImage}-x3.${extension} 3x"
+      alt="${alt != null ? alt : nameImage}"
+      />`;
+  }
   return `<img src="./assets/${nameImage}.${extension}" class="${className}" alt="${alt != null ? alt : nameImage}"/>`;
 }
