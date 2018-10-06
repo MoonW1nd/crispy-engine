34e6efad6528a1988ef95a76a5d529ee4c01ad48
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-10-07 01:36:01 +0300

    Add spread to end Title

--- src/blocks/_helpers/_helpers.js
+++ src/blocks/_helpers/_helpers.js
@@ -1,5 +1,4 @@
-/* global document */
-
+/* global document window */
 export function getTemplateContent(templateClassName) {
   const template = document.getElementsByClassName(templateClassName)[0];
   return document.importNode(template.content, true);
@@ -17,4 +16,32 @@ export function getImageHtml(nameImage, extension, className, alt = null) {
       />`;
   }
   return `<img src="./assets/${nameImage}.${extension}" class="${className}" alt="${alt != null ? alt : nameImage}"/>`;
+}
+
+
+export function getTruncateHandler(element) {
+  const text = element.innerHTML;
+  const title = element;
+  return () => {
+    const clone = document.createElement('div');
+
+    clone.style.position = 'absolute';
+    clone.style.visibility = 'hidden';
+
+    clone.style.width = `${title.clientWidth - 16}px`;
+    clone.style.fontSize = window.getComputedStyle(title).fontSize;
+    clone.innerHTML = 'test <br/> text';
+
+    document.body.appendChild(clone);
+    const targetHeight = clone.clientHeight;
+
+    clone.innerHTML = text;
+
+    let l = text.length - 1;
+    for (; l >= 0 && clone.clientHeight > targetHeight; --l) { //eslint-disable-line
+      clone.innerHTML = `${text.substring(0, l)}...`;
+    }
+
+    title.innerHTML = clone.innerHTML;
+  };
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /* global document window */
 import { render } from './blocks/Card/Card';
 import swipeEvent from './blocks/_helpers/_events';
+import { getTruncateHandler } from './blocks/_helpers/_helpers';
 
 
 const data = require('./data/events.json');
@@ -12,7 +13,35 @@ window.onload = () => {
     render(parent, eventData);
   });
 
+  const truncateHandlers = [];
+
+  Array.from(parent.querySelectorAll('.Title-Content')).forEach((title) => {
+    const truncate = getTruncateHandler(title);
+    truncate();
+    truncateHandlers.push(truncate);
+  });
   const element = parent.querySelector('.Article-Data_type_image img');
 
   swipeEvent(element);
+
+  // оптимизация resize событий
+  (function () { //eslint-disable-line
+    function actualResizeHandler() {
+      truncateHandlers.forEach((truncateHandler) => {
+        truncateHandler();
+      });
+    }
+
+    let resizeTimeout;
+    function resizeThrottler() {
+      if (!resizeTimeout) {
+        resizeTimeout = setTimeout(() => {
+          resizeTimeout = null;
+          actualResizeHandler();
+        }, 66);
+      }
+    }
+
+    window.addEventListener('resize', resizeThrottler, false);
+  }());
 };
