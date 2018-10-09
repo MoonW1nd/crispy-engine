bb7e61270939e042edb3b6d833704559be0861c7
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-10-10 00:20:48 +0300

    Multimedia [homework]: split homework html on chunks, add Footer/Header, update for new project sturcture

---
+++ src/pages/cctv/main.cctv.js
@@ -0,0 +1,40 @@
+/* global document window Hls */
+window.addEventListener('load', () => {
+  function initVideo(videoElement, url) {
+    const video = videoElement;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        video.play();
+      });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
+      video.addEventListener('loadedmetadata', () => {
+        video.play();
+      });
+    }
+  }
+
+  initVideo(
+    document.getElementById('video-1'),
+    'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8',
+  );
+
+  initVideo(
+    document.getElementById('video-2'),
+    'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fcat%2Fmaster.m3u8',
+  );
+
+  initVideo(
+    document.getElementById('video-3'),
+    'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fdog%2Fmaster.m3u8',
+  );
+
+  initVideo(
+    document.getElementById('video-4'),
+    'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fhall%2Fmaster.m3u8',
+  );
+});
