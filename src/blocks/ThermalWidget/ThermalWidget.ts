adcfd33aa2241b8453f4612974b981879c37040a
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-10-28 19:20:36 +0300

    TypeScript [homework]: typing ThermalWidget

--- src/blocks/ThermalWidget/ThermalWidget.ts
+++ src/blocks/ThermalWidget/ThermalWidget.ts
@@ -1,11 +1,18 @@
 /* global document */
 import { getTemplateContent } from '../_helpers/_helpers';
+import { ICardDataWeather } from '../Card/Card';
 
-export default function renderThermalWidget(parent, data) {
+export default function renderThermalWidget(parent: HTMLElement, data: ICardDataWeather) {
   const templateContent = getTemplateContent('ThermalWidgetTemplate');
+  const temperatureElement = templateContent.querySelector('.ThermalWidget-TemperatureValue');
+  const humidityElement = templateContent.querySelector('.ThermalWidget-HumidityValue');
 
-  templateContent.querySelector('.ThermalWidget-TemperatureValue').innerHTML = data.temperature;
-  templateContent.querySelector('.ThermalWidget-HumidityValue').innerHTML = data.humidity;
+  if (temperatureElement && humidityElement) {
+    temperatureElement.innerHTML = String(data.temperature);
+    humidityElement.innerHTML = String(data.humidity);
+  } else {
+    throw Error('Не корректный шаблон');
+  }
 
   const clone = document.importNode(templateContent, true);
   parent.appendChild(clone);
